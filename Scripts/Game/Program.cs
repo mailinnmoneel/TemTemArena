c0a1b20ad1f06f161c660fbdf7654da2813cb5dd
mailinnmoneel <mailinn.kiste@gmail.com>
2021-11-15 08:39:51 +0100

    x

--- Scripts/Game/Program.cs
+++ Scripts/Game/Program.cs
@@ -10,25 +10,34 @@ namespace TemTemArena
 {
     class Program
     {
+        //-- ASCII SYMBOLS
+        //-- https://www.alt-codes.net/
         static void Main(string[] args)
         {
-            var useGUI = true;
-            Game.Manager.Renderer = new GUIRenderer(useGUI);
-            Game.Manager.Renderer.PrintAllSymbols();
-            Game.Manager.Renderer.AddMessage(Align.Left, Messages.Welcome);
+            #region Instantiate Controller Classes
+            Game.Manager.EventLog = new EventLog();
+            Game.Manager.ScreenBuffer = new ScreenBuffer();
+            Game.Manager.Renderer = new GUIRenderer();
+            Game.Manager.Renderer.Preview();
 
-            Arena arena = new Arena();
+            GUI.AddEntry(EntryType.Header, Messages.GameHeader);
+            GUI.Refresh();
+            #endregion
+
+            Arena arena = new Arena(); //-- Main Game Controller
 
             #region CreateTemTems
-            TemTemDex.TemTemListe.ActiveTemTems.Add(new Player("Tateru", 78.5f, 20.5f, 85f, false));
-            TemTemDex.TemTemListe.ActiveTemTems.Add(new NPCTemTem("Ganki", 78.5f, 50.5f, 98f, false));
+            TemTemDex.TemTemListe.ActiveTemTems.Add(new Player("Tateru", 79f, 78f, 85f, false)); //Neutral
+            TemTemDex.TemTemListe.ActiveTemTems.Add(new Player("Nessla", 50f, 76f, 58f, false)); //Electric+ Water
+            TemTemDex.TemTemListe.ActiveTemTems.Add(new Player("Deendre", 75f, 48f, 42f, false)); //Neutral
+            TemTemDex.TemTemListe.ActiveTemTems.Add(new NPCTemTem("Momo", 75f, 46f, 64f, false)); //Normal
+            TemTemDex.TemTemListe.ActiveTemTems.Add(new NPCTemTem("Ganki", 38f, 57f, 46f, false)); //Electriv+Wind
+            TemTemDex.TemTemListe.ActiveTemTems.Add(new NPCTemTem("Oceara", 64f, 54f, 42f, false)); //Water
             #endregion
-      
+
             arena.TemTemBattle();
 
-            //1. Stelle ITemtem
-            //2. Lage to nye temtemklasser som arver fra TemTem
-            //3. Lage to nye interface, 1 for dine TemTems og en for NPCTemTem
+        
         }
     }
 }
